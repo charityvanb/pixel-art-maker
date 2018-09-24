90c825f55bb717ed62c22626143502ab2d6baf27
charityvanb <charityvanb@gmail.com>
2018-09-24 16:04:51 -0600

    Made paint and indicator functions

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var canvas = document.getElementById('canvas')
+var palette = document.getElementById('palette')
+var indicator = document.getElementById('indicator')
+var blueviolet = document.getElementById('blueviolet')
+var red = document.getElementById('red')
+var orange = document.getElementById('orange')
+var yellow = document.getElementById('yellow')
+var green = document.getElementById('green')
 
 for (var i = 0; i < 841; i++) {
     var pixel = document.createElement('div')
-    // Then pixel isn't available elsewhere
     pixel.classList.add('pixel')
     canvas.appendChild(pixel)
+    pixel.addEventListener('click', paintPixel)
     }
 
-for (var i = 0; i < 841; i++) {
-    pixel[i].addEventListener('click', turnRed)
+function paintPixel(event) {
+    event.target.style.backgroundColor = indicator.style.backgroundColor
+  
 }
 
-function turnRed(event) {
-    event.target.style.backgroundColor === 'red'
+blueviolet.addEventListener('click', getColor)
+red.addEventListener('click', getColor)
+orange.addEventListener('click', getColor)
+yellow.addEventListener('click', getColor)
+green.addEventListener('click', getColor)
+
+//how does the event thing in the parentheses work and when do I need it?
+function getColor(event) {
+    indicator.style.backgroundColor = event.target.id
+
 }
 
-var palette = document.getElementById('palette')
-
-
-//make an array of colors and iterate through
-// do that thing from the udemy lecture to change them
-
-// differentiate between the palette and canvas divs
